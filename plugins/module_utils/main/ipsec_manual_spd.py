4e7b55bf460e8a49b5bb2886607f5051be55bd88
Marius Rieder <marius.rieder@durchmesser.ch>
2025-01-24 17:10:55 +0100

    Implement module for ipsec_manual_spd

---
+++ plugins/module_utils/main/ipsec_manual_spd.py
@@ -0,0 +1,64 @@
+from ansible.module_utils.basic import AnsibleModule
+
+from ansible_collections.ansibleguy.opnsense.plugins.module_utils.base.api import \
+    Session
+from ansible_collections.ansibleguy.opnsense.plugins.module_utils.helper.main import \
+    validate_int_fields
+from ansible_collections.ansibleguy.opnsense.plugins.module_utils.base.cls import BaseModule
+
+
+class ManualSPD(BaseModule):
+    FIELD_ID = 'name'
+    CMDS = {
+        'add': 'add',
+        'del': 'del',
+        'set': 'set',
+        'search': 'search',
+        'detail': 'get',
+        'toggle': 'toggle',
+    }
+    API_KEY_PATH = 'spd'
+    API_MOD = 'ipsec'
+    API_CONT = 'manual_spd'
+    API_CONT_REL = 'service'
+    FIELDS_CHANGE = ['request_id', 'connection_child', 'source', 'destination', 'name']
+    FIELDS_ALL = ['enabled']
+    FIELDS_ALL.extend(FIELDS_CHANGE)
+    FIELDS_TRANSLATE = {
+        'name': 'description',
+        'request_id': 'reqid',
+    }
+    FIELDS_TYPING = {
+        'bool': ['enabled'],
+        'list': [],
+        'select': [],
+        'int': ['request_id'],
+    }
+    INT_VALIDATIONS = {
+        'request_id': {'min': 1, 'max': 65535},
+    }
+    EXIST_ATTR = 'spd'
+
+    def __init__(self, module: AnsibleModule, result: dict, session: Session = None):
+        BaseModule.__init__(self=self, m=module, r=result, s=session)
+        self.spd = {}
+        self.existing_childs = None
+
+    def check(self) -> None:
+        if self.p['state'] == 'present':
+            validate_int_fields(module=self.m, data=self.p, field_minmax=self.INT_VALIDATIONS)
+
+        self._base_check()
+
+    def _build_request(self) -> dict:
+        self.b.find_single_link(
+            field='connection_child',
+            existing=self.search_connection_child(),
+            existing_field_id='value',
+        )
+        return self.b.build_request()
+
+    def search_connection_child(self) -> dict:
+        return self.s.get(cnf={
+            **self.call_cnf, **{'command': self.CMDS['detail']}
+        })['spd']['connection_child']
