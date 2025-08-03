214b48551d063085e48cb63cb7090655ab40fd45
Rath Pascal <rath@oxl.at>
2025-08-03 19:09:48 +0200

    ci: add draft for running functional-tests for PRs

---
+++ scripts/test_pr.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+echo ''
+
+DEBUG=false
+TMP_DIR="/tmp/.opnsense_test_$(date +%s)"
+TMP_COL_DIR="${TMP_DIR}/collections"
+
+export ANSIBLE_INVENTORY_UNPARSED_WARNING=False
+export ANSIBLE_LOCALHOST_WARNING=False
+export ANSIBLE_NO_TARGET_SYSLOG=True
+
+LOCAL_COLLECTION="$(pwd)"
+VERBOSITY=''
+export SUCCEEDED=''
+export FAILED=''
+EXIT_CODE=0
+
+set -u
+
+source "$(dirname "$0")/test_prep.sh"  # shared between single/multi test
+
+function run_test_soft() {
+  module="$1"
+  check_mode="$2"
+  if run_test "$module" "$check_mode"
+  then
+    if [[ -n "$SUCCEEDED" ]]
+    then
+      export SUCCEEDED="${SUCCEEDED}, "
+    fi
+    export SUCCEEDED="${SUCCEEDED}${module}"
+  else
+    if [[ -n "$FAILED" ]]
+    then
+      export FAILED="${FAILED}, "
+    fi
+    export FAILED="${FAILED}${module}"
+    EXIT_CODE=1
+  fi
+}
+
+cd "$TMP_COL_DIR/ansible_collections/ansibleguy/opnsense"
+
+echo ''
+echo '##############################'
+echo '        STARTING TESTS!'
+echo '##############################'
+echo ''
+
+run_test_soft '1_dependencies' 0
+
+while read m
+do
+  run_test_soft "$m" 1
+done < /tmp/changed_modules.txt
+
+echo ''
+echo '##############################'
+echo '        FINISHED TESTS!'
+echo '##############################'
+echo ''
+
+echo '##############################'
+echo '           RESULTS:'
+if echo "$USER" | grep -q 'test'
+then
+  echo "SUCCEEDED: ${SUCCEEDED}"
+  echo "FAILED: ${FAILED}"
+else
+  printf "\033[0;32mSUCCEEDED: ${SUCCEEDED}\033[0m\n"
+  printf "\033[0;31mFAILED: ${FAILED}\033[0m\n"
+fi
+
+echo '##############################'
+
+rm -rf "$TMP_DIR"
+
+exit "$EXIT_CODE"
