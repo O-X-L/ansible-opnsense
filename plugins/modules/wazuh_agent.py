c7982462125bbbd214f92a2d8934cee88021d6cc
Maxime Wewer <maxime.wewer@agoracalyce.com>
2025-10-04 21:43:07 +0200

    chore: change collection namespace (ansibleguy => oxlorg)

--- plugins/modules/wazuh_agent.py
+++ plugins/modules/wazuh_agent.py
@@ -5,14 +5,14 @@
 
 from ansible.module_utils.basic import AnsibleModule
 
-from ansible_collections.ansibleguy.opnsense.plugins.module_utils.base.handler import \
+from ansible_collections.oxlorg.opnsense.plugins.module_utils.base.handler import \
     module_dependency_error, MODULE_EXCEPTIONS
 
 try:
-    from ansible_collections.ansibleguy.opnsense.plugins.module_utils.helper.wrapper import module_wrapper
-    from ansible_collections.ansibleguy.opnsense.plugins.module_utils.defaults.main import \
+    from ansible_collections.oxlorg.opnsense.plugins.module_utils.helper.wrapper import module_wrapper
+    from ansible_collections.oxlorg.opnsense.plugins.module_utils.defaults.main import \
         OPN_MOD_ARGS, EN_ONLY_MOD_ARG, RELOAD_MOD_ARG
-    from ansible_collections.ansibleguy.opnsense.plugins.module_utils.main.wazuh_agent import WazuhAgent
+    from ansible_collections.oxlorg.opnsense.plugins.module_utils.main.wazuh_agent import WazuhAgent
 
 except MODULE_EXCEPTIONS:
     module_dependency_error()
